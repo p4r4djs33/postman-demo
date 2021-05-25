c9dde8091373fed87f4413224494253794a9939a
Kien <tata.vutrungkien@gmail.com>
2021-05-25 17:32:17 +0700

    init

---
+++ src/main/java/com/example/postmandemo/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.example.postmandemo.service.customer;
+
+import com.example.postmandemo.model.Customer;
+import com.example.postmandemo.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
